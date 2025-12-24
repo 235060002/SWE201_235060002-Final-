f8e886be78de1de46b6ac204c24fadf9a2ce1e5d
Aday AKYÜZ <235060002@toros.edu.tr>
2025-12-24 16:32:48 +0300

    Borrow.cs

--- Borrow.cs
+++ Borrow.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWE201_235060002
+public class Borrow
 {
-    internal class Class1
+    public Book BorrowedBook { get; set; }
+    public Member Member { get; set; }
+    public int BorrowDays { get; set; }
+
+    public Borrow(Book book, Member member, int borrowDays)
     {
+        BorrowedBook = book;
+        Member = member;
+        BorrowDays = borrowDays;
+        book.BorrowBook();
+    }
+
+    public void ShowBorrowInfo()
+    {
+        Console.WriteLine($"Member: {Member.FullName}");
+        Console.WriteLine($"Book: {BorrowedBook.Title}");
+        Console.WriteLine($"Author: {BorrowedBook.Author}");
+        Console.WriteLine($"Borrow Days: {BorrowDays}");
+        Console.WriteLine($"Book Available: {BorrowedBook.IsAvailable}");
     }
 }
